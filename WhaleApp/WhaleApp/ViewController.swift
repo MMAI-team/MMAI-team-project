7d15761d5265b235e48905f1b070c37026604c33
Yurii Pobeerzhnyi <yurapoberezhnyy@gmail.com>
2023-11-29 15:33:45 +0200

    Initial app commit

---
+++ WhaleApp/WhaleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhaleApp
+//
+//  Created by Yurii Poberezhnyi on 29.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
