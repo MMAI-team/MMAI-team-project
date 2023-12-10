e416ca82beb62a9965a219cabfe3fe0f30ba66e3
Lubomyr <poixon2001@gmail.com>
2023-12-10 22:52:42 +0200

    feat: add model

--- flask-api/main.py
+++ flask-api/main.py
@@ -1,51 +1,38 @@
 from flask import Flask, request, jsonify
+import base64
+import io
+from PIL import Image
+import numpy as np
 from models import PathModel
-from werkzeug.utils import secure_filename
-import os
 
 app = Flask(__name__)
 
-# Configuration
-MODEL_PATH = "path/to/model"
+# Model configuration
+MODEL_PATH = "C:/Users/LiubomyrMaievskyi/Desktop/CNN.h5"
 model = PathModel(MODEL_PATH, "CNN model")
-UPLOAD_FOLDER = "uploads/"
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
-
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-def allowed_file(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
 
+    # Decode the first image
+    image_data1 = base64.b64decode(data["image1"])
+    image1 = Image.open(io.BytesIO(image_data1))
 
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    # Check if the post request has the file part
-    if "file1" not in request.files or "file2" not in request.files:
-        return "No file part"
-    file1 = request.files["file1"]
-    file2 = request.files["file2"]
+    # Decode the second image
+    image_data2 = base64.b64decode(data["image2"])
+    image2 = Image.open(io.BytesIO(image_data2))
 
-    # If user does not select file, browser submits an empty part without filename
-    if file1.filename == "" or file2.filename == "":
-        return "No selected file"
-    if (
-        file1
-        and file2
-        and allowed_file(file1.filename)
-        and allowed_file(file2.filename)
-    ):
-        filename1 = secure_filename(file1.filename)
-        filename2 = secure_filename(file2.filename)
-        file_path1 = os.path.join(app.config["UPLOAD_FOLDER"], filename1)
-        file_path2 = os.path.join(app.config["UPLOAD_FOLDER"], filename2)
-        file1.save(file_path1)
-        file2.save(file_path2)
+    # Convert images to a format your model expects (e.g., numpy array)
+    # This conversion depends on your model's requirement
+    image_array1 = np.array(image1)
+    image_array2 = np.array(image2)
 
-        # Process images with model
-        result = model.predict([file_path1, file_path2])
+    # Process images with the model
+    result = model.predict([image_array1, image_array2])
 
-        return jsonify(result)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
